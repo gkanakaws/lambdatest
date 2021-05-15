f3e5fc78fb90801fe92da6fa358e863ac6e79daf
gkanakaws <58528839+gkanakaws@users.noreply.github.com>
2021-05-15 15:20:56 -0700

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,8 @@
 import json
+import boto3
+import git 
+
+codepipeline_client = boto3.client('codepipeline')
 
 def lambda_handler(event, context):
     # TODO implement
@@ -6,3 +10,10 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda-auto-new!')
     }
+
+    #Get commit differences
+    commit = git.commit('master')
+    print(commit)
+
+
+# https://gitpython.readthedocs.io/en/stable/tutorial.html
